9be8f8832536b368bc6acc06dfaf84ed83c05cf5
createaniket <aniketpratapsingh7310@gmail.com>
2024-07-25 09:31:16 +0530

    :

--- src/layouts/AdminLayout/NavBar/NavRight/ChatList/Friends/Chat/chat.jsx
+++ src/layouts/AdminLayout/NavBar/NavRight/ChatList/Friends/Chat/chat.jsx
@@ -1,5 +1,5 @@
 // assets
-import avatar1 from 'assets/images/user/avatar-1.jpg';
+import avatar1 from 'assets/images/user/tricolor.png';
 import avatar2 from 'assets/images/user/avatar-2.jpg';
 import avatar3 from 'assets/images/user/avatar-3.jpg';
 import avatar4 from 'assets/images/user/avatar-4.jpg';
