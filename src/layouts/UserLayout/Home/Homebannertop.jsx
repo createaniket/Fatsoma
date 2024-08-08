47fc952691d137dbca7b8809bdd105a899100792
Mac <mac@Macs-MacBook-Pro.local>
2024-08-08 20:12:33 +0530

    Auto scroll to top Bug Fixed

--- src/layouts/UserLayout/Home/Homebannertop.jsx
+++ src/layouts/UserLayout/Home/Homebannertop.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from '../CarouselTemplate/templateimage';
-import HomeTopBannerOne from '../../../assets/images/Home/Home_Top_Banner_one.jpg'
-import HomeTopBannertwo from '../../../assets/images/Home/Home_banner_two.jpg'
-import HomeTopBannerthree from '../../../assets/images/Home/Home_Banner_three.jpg'
-import './Homebannertop.scss'
+import HomeTopBannerOne from '../../../assets/images/Home/Home_Top_Banner_one.jpg';
+import HomeTopBannertwo from '../../../assets/images/Home/Home_banner_two.jpg';
+import HomeTopBannerthree from '../../../assets/images/Home/Home_Banner_three.jpg';
+import './Homebannertop.scss';
 
 const Homebannertop = () => {
   return (
     <div>
-      <Carousel>
-        <Carousel.Item>
+      <Carousel
+        interval={null}          // Disable auto-slide
+        keyboard={false}         // Disable keyboard navigation
+        controls={false}         // Disable carousel controls (arrows)
+        indicators={false}       // Disable slide indicators (dots)
+      >
+        <Carousel.Item tabIndex="-1">  {/* Prevent focus on slide change */}
           <ExampleCarouselImage 
             imageSrc={HomeTopBannerOne}
             text="First slide" 
           />
-
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item tabIndex="-1">  {/* Prevent focus on slide change */}
           <ExampleCarouselImage 
             imageSrc={HomeTopBannertwo}
             text="Second slide" 
           />
-
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item tabIndex="-1">  {/* Prevent focus on slide change */}
           <ExampleCarouselImage 
             imageSrc={HomeTopBannerthree}
             text="Third slide" 
           />
-
         </Carousel.Item>
       </Carousel>
     </div>
